d58b0a4fe40caa10e71a75aeb61127978adcc9c8
Matt West <matt@codeslo.com>
2017-07-18 10:11:44 -0700

    Added controllers and views for connections and connected devices

---
+++ NetworkBillingSystem_Alpha/Controllers/ConnectedDevicesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NetworkBillingSystem_Alpha.DAL;
+using NetworkBillingSystem_Alpha.Models;
+
+namespace NetworkBillingSystem_Alpha.Controllers
+{
+    public class ConnectedDevicesController : Controller
+    {
+        private ApplicationContext db = new ApplicationContext();
+
+        // GET: ConnectedDevices
+        public ActionResult Index()
+        {
+            return View(db.ConnectedDevices.ToList());
+        }
+
+        // GET: ConnectedDevices/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ConnectedDevice connectedDevice = db.ConnectedDevices.Find(id);
+            if (connectedDevice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(connectedDevice);
+        }
+
+        // GET: ConnectedDevices/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: ConnectedDevices/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ConnectedDevice connectedDevice = db.ConnectedDevices.Find(id);
+            if (connectedDevice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(connectedDevice);
+        }
+
+        // POST: ConnectedDevices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ConnectedDevice connectedDevice = db.ConnectedDevices.Find(id);
+            db.ConnectedDevices.Remove(connectedDevice);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NetworkBillingSystem_Alpha/Controllers/ConnectionsController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NetworkBillingSystem_Alpha.DAL;
+using NetworkBillingSystem_Alpha.Models;
+
+namespace NetworkBillingSystem_Alpha.Controllers
+{
+    public class ConnectionsController : Controller
+    {
+        private ApplicationContext db = new ApplicationContext();
+
+        // GET: Connections
+        public ActionResult Index()
+        {
+            var connections = db.Connections.Include(c => c.BDI).Include(c => c.ConnectedDevice).Include(c => c.ReportingDevice);
+            return View(connections.ToList());
+        }
+
+        // GET: Connections/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Connection connection = db.Connections.Find(id);
+            if (connection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(connection);
+        }
+
+        // GET: Connections/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Connection connection = db.Connections.Find(id);
+            if (connection == null)
+            {
+                return HttpNotFound();
+            }
+            return View(connection);
+        }
+
+        // POST: Connections/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Connection connection = db.Connections.Find(id);
+            db.Connections.Remove(connection);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
